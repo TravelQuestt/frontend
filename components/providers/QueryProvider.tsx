092eb912549c41fe0a26e5f55fd93949ecdfc06a
resist15 <sourav@aospa.co>
2025-12-14 13:13:07 +0530

    TravelQuest: StrongType QueryProvider functions

--- components/providers/QueryProvider.tsx
+++ components/providers/QueryProvider.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
 import { ReactNode, useState } from "react";
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
-    const isUnauthorized = (error) => {
-        return error?.response?.status === 401;
+    const isUnauthorized = (error: unknown): boolean => {
+        return (
+            axios.isAxiosError(error) &&
+            error.response?.status === 401
+        );
     };
 
     const [queryClient] = useState(() => new QueryClient({
